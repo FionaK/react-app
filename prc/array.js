7dc2a3377a3e3e7747d1a99c90e6ceb24f117f6d
Fiona K Ouwor <fiona@techfactory.ltd>
2020-04-16 22:02:38 +0300

    pass an array as props

---
+++ prc/array.js
@@ -0,0 +1,26 @@
+const List = (props) => {
+
+  return <p>{props.tasks.join(", ")}</p>
+  
+};
+
+class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>To Do Lists</h1>
+        <h2>Today</h2>
+        <List tasks = {["code", "eat"]}/>
+        
+
+        
+        <h2>Tomorrow</h2>
+        <List tasks = {["run", "jog", "exercise"]}/>
+        
+      </div>
+    )
+  }
+};
