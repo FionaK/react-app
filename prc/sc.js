c9427f40c016f11226bbd3769a6e36e94673a6fb
Fiona K Ouwor <fiona@techfactory.ltd>
2020-04-22 21:31:29 +0300

    update file

--- prc/sc.js
+++ prc/sc.js
@@ -25,7 +25,7 @@ class ResetPassword extends React.Component {
           <h2>Reset Password</h2>
           <h3>We've generated a new temporary password for you.</h3>
           <h3>Please reset this password from your account settings ASAP.</h3>
-          { /* change code below this line */ }
+     
           <ReturnTempPassword tempPassword= 'fionakat'/>
 
           { /* change code above this line */ }
