1ecb57f96109fc1c19c6753d88cdc02e80b806ca
FionaK <katefiona121@gmail.com>
2020-05-06 09:34:19 +0300

    fix typo

---
+++ prc/t3.js
@@ -0,0 +1,20 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'test'
+    }
+  }
+  render() {
+    // change code below this line
+    const name = this.state.name;
+
+    return (
+      <div>
+        { /* change code below this line */ }
+        <h1>{name}</h1>
+
+      </div>
+    );
+  }
+};
