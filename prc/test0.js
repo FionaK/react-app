e73fb5819c529dcef9fa7c7f8ea9ae3af543aef0
FionaK <katefiona121@gmail.com>
2020-05-06 09:30:03 +0300

    add file

---
+++ prc/test0.js
@@ -0,0 +1,25 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "Hello"
+    };
+    // change code below this line
+    this.handleClick = 
+    this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState({
+      text: "You clicked!"
+    });
+  }
+  render() {
+    return (
+      <div>
+        { /* change code below this line */ }
+        <button onClick={this.handleClick}>Click Me</button>
+        <h1>{this.state.text}</h1>
+      </div>
+    );
+  }
+};
