ac7691d7ae25e99583ac6aa7c5199a2b7ad4aea3
FionaK <katefiona121@gmail.com>
2020-05-28 07:18:26 +0300

    lifecycle method componentWillMount

---
+++ prc/t14.js
@@ -0,0 +1,13 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // change code below this line
+    console.log(name);
+
+  }
+  render() {
+    return <div />
+  }
+};
